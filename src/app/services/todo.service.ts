9162eadaf3c3a996f65fe9e778707217eeceaa8a
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-08-14 19:42:54 +0200

    catch if todo is completed in component html

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
 })
 export class TodoService {
   todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
-  todosLimit = '?_limit=3';
+  todosLimit = '?_limit=5';
 
   constructor(private http: HttpClient) { }
 
