4c9b73a3aae2cbcc903d34aa10fb0e20d2fa87ab
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-08-15 14:52:54 +0200

    Added add-todo service and ui

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -25,4 +25,10 @@ export class TodosComponent implements OnInit {
     this.todoService.deleteTodo(todo).subscribe();
   }
 
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
+  }
+
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -29,6 +29,11 @@ export class TodoService {
     return this.http.delete<Todo>(url, httpOptions);
   }
 
+  // Add Todo
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
+
   // Toggle Completed
   toggleCompleted(todo: Todo): Observable<any> {
     const url = `${this.todosUrl}/${todo.id}`;
